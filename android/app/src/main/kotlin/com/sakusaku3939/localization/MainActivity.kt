c8413f84df020765a1dfdce063de7c2743cd19cd
sakusaku3939 <yuukiyuuki223@gmail.com>
2023-08-22 15:51:34 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/com/sakusaku3939/localization/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.sakusaku3939.localization
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constant/custom_color.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class CustomColor {
+  static ColorScheme colorScheme = const ColorScheme.light(
+      primary: primary, onPrimary: lightestGrey, surface: lightestGrey);
+  static const Color primary = Color(0xff546E7A);
+  static const Color lightestGrey = Color(0xffFAFAFA);
+}
