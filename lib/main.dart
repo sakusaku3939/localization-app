866f0c01bed8df822a95c57d12b1a96cc69f8b8b
sakusaku3939 <yuukiyuuki223@gmail.com>
2023-08-29 05:58:39 +0900

    Add hooks and provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:localization/constant/color_palette.dart';
 import 'package:localization/view/home.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
