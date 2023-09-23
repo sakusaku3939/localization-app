4692c214d74c99e4152eb58a91f4e9305650cb43
sakusaku3939 <yuukiyuuki223@gmail.com>
2023-09-23 21:11:54 +0900

    Add settings for firebase storage

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:localization/constant/color_palette.dart';
 import 'package:localization/view/home.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     const ProviderScope(
       child: MyApp(),
