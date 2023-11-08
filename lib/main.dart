4951ea43c86d5e2e8e1f6fbccefa2120f3ad6bbb
sakusaku3939 <yuukiyuuki223@gmail.com>
2023-11-09 01:06:57 +0900

    Fix a bug of Japanese font

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:localization/constant/color_palette.dart';
 import 'package:localization/constant/global_context.dart';
 import 'package:localization/view/home.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
@@ -29,6 +30,14 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorPalette.colorScheme,
         useMaterial3: true,
       ),
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale("ja", "JP"),
+      ],
       home: const Home(),
     );
   }
